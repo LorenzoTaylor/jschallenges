62a92191ca757222894635842486c583791ea338
LorenzoTaylor <lmtaylor9@dons.usfca.edu>
2024-12-11 20:56:59 -0800

    solved 06

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,13 @@
-function titleCase() {}
+function titleCase(phrase) {
+    var newPhrase = "";
+    for(let i = 0 ; i < phrase.length ; i++){
+        if(phrase[i-1] == " " || i == 0){
+            newPhrase += phrase[i].toUpperCase();
+        } else {
+            newPhrase += phrase[i];
+        }
+    }
+    return newPhrase;
+}
 
 module.exports = titleCase;
