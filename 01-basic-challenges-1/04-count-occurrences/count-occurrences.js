bfa2e1ca215b828ddf78b8338ff00592a74438f5
LorenzoTaylor <lmtaylor9@dons.usfca.edu>
2024-12-11 20:56:05 -0800

    solved 04

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,11 @@
-function countOccurrences() {}
+function countOccurrences(word, character) {
+    var count = 0;
+    for(let i = 0 ; i < word.length ; i++){
+        if(word[i] == character){
+            count++;
+        }
+    }
+    return count;
+}
 
 module.exports = countOccurrences;
