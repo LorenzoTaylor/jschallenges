0cde63cf9cadbd0bc4ccdc1e4d52b6030607ce8c
LorenzoTaylor <lmtaylor9@dons.usfca.edu>
2024-12-11 20:56:26 -0800

    solved 05

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,14 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+    if (Array.isArray(arr) == false){
+        throw new Error("Man do better")
+    }
+    var max = Number.MIN_VALUE;
+    for (let i = 0 ; i < arr.length ; i++){
+        if (arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
 
 module.exports = findMaxNumber;
